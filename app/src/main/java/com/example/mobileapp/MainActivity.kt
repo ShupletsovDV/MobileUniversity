ae359afd25f299394fc88cb0e42e06608713fd90
Danil <shupletsovdanil2001@gmail.com>
2026-09-14 14:19:10 +0700

    DemoSlider

--- app/src/main/java/com/example/mobileapp/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp/MainActivity.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.mobileapp.ui.theme.MobileAppTheme
 
@@ -36,6 +38,16 @@ fun DemoText(message: String, fontSize: Float) {
         text = message,
         fontSize = fontSize.sp,
         fontWeight = FontWeight.Bold
+    )
+}
+
+@Composable
+fun DemoSlider(sliderPosition: Float, onPositionChange: (Float) -> Unit) {
+    Slider(
+        modifier = Modifier.padding(10.dp),
+        valueRange = 20f..38f,
+        value = sliderPosition,
+        onValueChange = { onPositionChange(it) }
     )
 }
 
